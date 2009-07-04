1d3f9fe2428224992e7dfa5d3baf512bf28ca58f
Keith Brister <k-brister@northwestern.edu>
2009-07-04 16:56:24 +0000

    removed print statement
    git-svn-id: file:///contrabass/repos/svn/cats/trunk@914 66ca1eeb-be3a-49d5-93d8-8bdade05efff

--- CatsOk.py
+++ CatsOk.py
@@ -493,7 +493,6 @@ class CatsOk:
             raise CatsOkError( 'Wrong number of arguments received in status state response: got %d, exptected 15 or 16' % (len(a)))
         #                            0            1            2             3           4          5   6   7   8   9  10   11         12           13           14
 
-        # print "Barcode: ", a[11]
         if self.statusStateLast == None or self.statusStateLast != s:
             b = []
             i = 0
