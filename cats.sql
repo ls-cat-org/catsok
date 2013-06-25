b193e79f169b0b5fd3801d09e6e37a6dbb63562c
Keith Brister <k-brister@northwestern.edu>
2013-06-25 14:26:35 +0000

    added GPL
    git-svn-id: file:///contrabass/repos/svn/cats/trunk@3137 66ca1eeb-be3a-49d5-93d8-8bdade05efff

--- cats.sql
+++ cats.sql
@@ -1,3 +1,26 @@
+--
+-- Support for data collection at LS-CAT
+--
+-- Copyright 2008-2013 by Keith Brister, Northwestern University
+--
+--   This file is part of the LS-CAT Beamline Control Package which is
+--   free software: you can redistribute it and/or modify it under the
+--   terms of the GNU General Public License as published by the Free
+--   Software Foundation, either version 3 of the License, or (at your
+--   option) any later version.
+--
+--   This software is distributed in the hope that it will be useful,
+--   but WITHOUT ANY WARRANTY; without even the implied warranty of
+--   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+--   General Public License for more details.
+--
+--   You should have received a copy of the GNU General Public License
+--   along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+--
+--
+-- Support for CATS robot control
+--
+--
 begin;
 DROP SCHEMA cats cascade;
 CREATE SCHEMA cats;
