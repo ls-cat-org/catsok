ac6e93bbc86043a42aee720f0d7edd22b0ca1df5
Jory Folker <38365438+jfolker@users.noreply.github.com>
2025-04-23 18:42:38 -0500

    Make a logging message less confusing.

--- CatsOk.py
+++ CatsOk.py
@@ -700,7 +700,7 @@ class CatsOk:
 
 
         if len(a) != 20:
-            print s
+            print "statusStateParse(%s)" % (s)
             raise CatsOkError( 'Wrong number of arguments received in status state response: got %d, exptected 20' % (len(a)))
         #                            0            1            2             3           4          5   6   7   8   9  10   11         12           13           14
 
